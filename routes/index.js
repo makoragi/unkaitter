41e143b0d59916f0dd721341531556b9c44f67a7
makoragi <makoragi@gmail.com>
2015-06-21 09:21:57 +0900

    キャプチャして保存

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
 
+var CronJob = require('cron').CronJob;
+var moment = require('moment');
+var fs = require('fs');
+var request = require('request');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Unkaitter' });
 });
+
+var cronTime = '0 0 7 * * *';
+
+new CronJob({
+	cronTime: cronTime,
+	onTick: function() {
+		tweet();
+
+		var picStream = fs.createWriteStream('img.jpg');
+		picStream.on('close', function(){
+			console.log('file done.');
+		});
+		picStream.on('error', function(err){
+			console.log(err);
+		});
+
+		request('http://today3.aso.ne.jp/SnapshotJPEG?Resolution=640x480&Quality=Clarity&View=Normal').pipe(picStream);
+	},
+	start: true
+});
+
+function tweet() {
+	var message = moment().utc().add(9, 'h').format("ただいま MM月DD日 HH時mm分です。");
+	console.log(message);
+}
 
 module.exports = router;
