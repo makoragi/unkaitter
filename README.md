8ce856bbea7c882687e955cf8390db8d643a5b65
Makoto Sakuragi(vaio) <makoragi@gmail.com>
2015-06-23 15:29:02 +0900

    Posting Tumblr

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # unkaitter
 unkai x twitter
+
+## .env
+
+cron='0 0 7 * * *'
+tempdir='/tmp'
+tumblrConsumerKey='xxxxxx'
+tumblrConsumerSecret='xxxxxx'
+tumblrAccessToken='xxxxxx'
+tumblrAccessSecret='xxxxxx'
--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,16 @@ var moment = require('moment');
 var fs = require('fs');
 var request = require('request');
 
+var Tumblr = require('tumblrwks');
+var tumblr = new Tumblr(
+  {
+    consumerKey: process.env.tumblrConsumerKey,
+    consumerSecret: process.env.tumblrConsumerSecret,
+    accessToken: process.env.tumblrAccessToken,
+    accessSecret: process.env.tumblrAccessSecret
+  }, 'unkaitter.tumblr.com'
+);
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.render('index', { title: 'Unkaitter' });
@@ -17,17 +27,32 @@ router.get('/', function(req, res, next) {
 
 var cronTime = process.env.cron || '0 0 7 * * *';
 
+var tempfile = process.env.tempdir + '/img.jpg';
+
 new CronJob({
 	cronTime: cronTime,
 	onTick: function() {
 		tweet();
 
-		var picStream = fs.createWriteStream(process.env.tempdir + '/img.jpg');
+		var picStream = fs.createWriteStream(tempfile);
 		picStream.on('close', function(){
-			console.log('file done.');
+
+			var photo = fs.readFileSync(tempfile);
+			tumblr.post('/post',
+			  {
+			    type: 'photo',
+			    data: [photo]
+			  }, function(err, json) {
+			    if (!err) {
+			      console.log(json);
+			    } else {
+			      console.error(err);
+			    }
+			  }
+			);
 		});
 		picStream.on('error', function(err){
-			console.log(err);
+			console.error(err);
 		});
 
 		request('http://today3.aso.ne.jp/SnapshotJPEG?Resolution=640x480&Quality=Clarity&View=Normal').pipe(picStream);
