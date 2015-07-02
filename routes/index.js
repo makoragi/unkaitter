83486ed682ec54395b51cb2d9d98a5683562af74
makoragi <makoragi@gmail.com>
2015-07-02 23:15:48 +0900

    気象情報も一緒にツイート

--- routes/index.js
+++ routes/index.js
@@ -36,9 +36,16 @@ router.get('/', function(req, res, next) {
   });
 });
 
-var cronTime = process.env.cron || '0 0 6-10 * * *';
-var cronTimeWet = process.env.cronWet || '0 0 6-10 * * *';
-
+var cronTime = process.env.cron || '0 0 6-10,22 * * *';
+var cronTimeWet = process.env.cronWet || '0 0 0-5,11-21,23 * * *';
+var urlWet = "http://www.tenki.jp/amedas/9/46/86156.html";
+// var urlWet = "http://www.tenkiaaaaaa.jp/amedas/9/46/86156.html"; //DEBUG
+var regexp_temp = /\D(\d+\.\d+)℃/;
+var regexp_wind = /\D(\d+\.\d+)m\/s/;
+var regexp_rain = /\D(\d+\.\d+)mm/;
+var temp = [];
+var wind = [];
+var rain = [];
 var tempfile = process.env.tempdir + '/img.jpg';
 
 new CronJob({
@@ -46,22 +53,31 @@ new CronJob({
 	onTick: function() {
 		tweet();
 
-		var picStream = fs.createWriteStream(tempfile);
-		picStream.on('close', function(){
-			tumblr_image();
-			tweet_image();
+		request({url: urlWet}, function(err, res, body) {
+			if (!err && res.statusCode == 200) {
+				get_weather(body);
+
+				var picStream = fs.createWriteStream(tempfile);
+				picStream.on('close', function(){
+					tumblr_image();
+					tweet_image();
+				});
+
+				picStream.on('error', function(err){
+					console.error(err);
+				});
+
+				request('http://today3.aso.ne.jp/SnapshotJPEG?Resolution=640x480&Quality=Clarity&View=Normal').pipe(picStream);
+
+			} else {
+				console.error(err);
+			}
 		});
 
-		picStream.on('error', function(err){
-			console.error(err);
-		});
-
-		request('http://today3.aso.ne.jp/SnapshotJPEG?Resolution=640x480&Quality=Clarity&View=Normal').pipe(picStream);
 	},
 	start: true
 });
 
-var urlWet = "http://www.tenki.jp/amedas/9/46/86156.html";
 new CronJobWet({
 	cronTime: cronTimeWet,
 	onTick: function() {
@@ -69,14 +85,14 @@ new CronJobWet({
 
 		request({url: urlWet}, function(err, res, body) {
 			if (!err && res.statusCode == 200) {
-				var $ = cheerio.load(body);
-				$("table[class=amedas_table_current]").first().children().each(function(){
-					//
-					console.log($(this).children());
-				});
+
+				get_weather(body);
+				tweet_weather();
+
+			} else {
+				console.error(err);
 			}
 		});
-		console.log("fin.");
 	},
 	start: true
 });
@@ -113,7 +129,12 @@ function tweet_image() {
 			if (!err) {
 				var mediaIdStr = data.media_id_string;
 				var params = {
-					status: moment().utc().add(9, 'h').format("ただいま MM月DD日 HH時mm分です。"),
+					// status: moment().utc().add(9, 'h').format("ただいま MM月DD日 HH時mm分です。"),
+					status: '現在の気温' + temp[0] + '℃ '
+						+ '24H以内の温度差' + (temp[1] - temp[2]).toFixed(1) + '℃ '
+						+ '現在の風速' + wind[0] + 'm/s '
+						+ '現在の降水量' + rain[1] + 'mm '
+						+ moment().utc().add(9, 'h').format("MM月DD日 HH時mm分"),
 					media_ids: [mediaIdStr]
 				};
 				Tw.post('statuses/update', params, function(err, data, response){
@@ -124,6 +145,62 @@ function tweet_image() {
 			}
 		}
 	);
+}
+
+function get_weather(body) {
+	var $ = cheerio.load(body);
+	$("table[class=amedas_table_current]").first().children().each(function(){
+		//// <tr>
+		$(this).children().each(function(i, elem){
+			//// <th> or <td>
+			// if (i == 1) {
+			// 	console.log($(this).prev().text());
+			// }
+			if (i == 1 && $(this).prev().text() == '気温') {
+				// console.log($(this));
+				temp = [];
+				$(this).find('li').each(function(j, elem){
+					if ((myArray = regexp_temp.exec($(this).text())) !== null) {
+						temp[j] = myArray[1];
+					}
+				});
+				// console.log('現在の気温' + temp[0] + '℃');
+				// console.log('24H以内の温度差' + (temp[1] - temp[2]).toFixed(1) + '℃');
+			} else if (i == 1 && $(this).prev().text() == '風向・風速') {
+				// console.log($(this));
+				wind = [];
+				$(this).find('li').each(function(j, elem){
+					if ((myArray = regexp_wind.exec($(this).text())) !== null) {
+						wind[j] = myArray[1];
+					}
+				});
+				// console.log('現在の風速' + wind[0] + 'm/s');
+			} else if (i == 1 && $(this).prev().text() == '降水量') {
+				// console.log($(this));
+				rain = [];
+				$(this).find('li').each(function(j, elem){
+					if ((myArray = regexp_rain.exec($(this).text())) !== null) {
+						rain[j] = myArray[1];
+					}
+				});
+				// console.log('現在の降水量' + rain[1] + 'mm');
+			}
+		});
+	});
+}
+
+function tweet_weather() {
+	var params = {
+		// status: moment().utc().add(9, 'h').format("ただいま MM月DD日 HH時mm分です。")
+		status: '現在の気温' + temp[0] + '℃ '
+			+ '24H以内の温度差' + (temp[1] - temp[2]).toFixed(1) + '℃ '
+			+ '現在の風速' + wind[0] + 'm/s '
+			+ '現在の降水量' + rain[1] + 'mm '
+			+ moment().utc().add(9, 'h').format("MM月DD日 HH時mm分")
+	};
+	Tw.post('statuses/update', params, function(err, data, response){
+		// console.log(data);
+	});
 }
 
 module.exports = router;
